5a4da9daf1e03116c2b26361aeb5ce9610ff5778
Kaznowski <kim@doubledashstudios.com>
2021-09-18 14:00:54 -0300

    + Create BoolReactives Mono and Scriptable Implementations

---
+++ Runtime/ReactiveVariable/Implementations/Bool/BoolReactiveMonoBehaviour.cs
@@ -0,0 +1,4 @@
+namespace DoubleDash.CodingTools.ReactiveVariables
+{
+    public class BoolReactiveMonoBehaviour : ReactiveVariableMonoBehaviour<bool> { }
+}
---
+++ Runtime/ReactiveVariable/Implementations/Bool/BoolReactiveScriptable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using DoubleDash.CodingTools.ReactiveVariables;
+using UnityEngine;
+
+[UnityEngine.CreateAssetMenu(fileName = "NewReactiveBool", menuName = "DoubleDash/ReactiveVariables/Bool")]
+public class BoolReactiveScriptable : ReactiveVariableScriptable<bool>
+{
+    
+}
