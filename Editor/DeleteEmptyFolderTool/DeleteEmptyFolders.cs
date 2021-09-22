fc0b35d6f6c27de735db471274625c947774342a
Kaznowski <kim@doubledashstudios.com>
2021-09-21 21:59:20 -0300

    + Add DeleteEmptyFolders Tool

---
+++ Editor/DeleteEmptyFolderTool/DeleteEmptyFolders.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace DoubleDash.DeleteEmptyDirTools
+{
+    [InitializeOnLoad]
+    public class DeleteEmptyFolders : UnityEditor.AssetModificationProcessor
+    {
+        const string CLEAN_ON_SAVE_KEY = "k1";
+        public static event Action OnAutoClean;
+
+        public static string[] OnWillSaveAssets(string[] paths)
+        {
+            if (CleanOnSave)
+            {
+                List<DirectoryInfo> emptyDirs;
+                FillEmptyDirList(out emptyDirs);
+                if (emptyDirs != null && emptyDirs.Count > 0)
+                {
+                    DeleteAllEmptyDirAndMeta(ref emptyDirs);
+
+                    Debug.Log("[DeleteEmptyDirTools] Cleaned Empty Directories on Save");
+
+                    if (OnAutoClean != null)
+                        OnAutoClean();
+                }
+            }
+
+            AssetDatabase.Refresh();
+            return paths;
+        }
+
+
+        public static bool CleanOnSave
+        {
+            get => EditorPrefs.GetBool(CLEAN_ON_SAVE_KEY, true);
+            set => EditorPrefs.SetBool(CLEAN_ON_SAVE_KEY, value);
+        }
+
+
+        public static void DeleteAllEmptyDirAndMeta(ref List<DirectoryInfo> emptyDirs)
+        {
+            foreach (var dirInfo in emptyDirs)
+            {
+                AssetDatabase.MoveAssetToTrash(GetRelativePathFromCd(dirInfo.FullName));
+            }
+
+            emptyDirs = null;
+        }
+
+        public static void FillEmptyDirList(out List<DirectoryInfo> emptyDirs)
+        {
+            var newEmptyDirs = new List<DirectoryInfo>();
+            emptyDirs = newEmptyDirs;
+
+            var assetDir = new DirectoryInfo(Application.dataPath);
+
+            WalkDirectoryTree(assetDir, (dirInfo, areSubDirsEmpty) =>
+            {
+                bool isDirEmpty = areSubDirsEmpty && DirHasNoFile(dirInfo);
+                if (isDirEmpty)
+                    newEmptyDirs.Add(dirInfo);
+                return isDirEmpty;
+            });
+        }
+
+        // return: Is this directory empty?
+        delegate bool IsEmptyDirectory(DirectoryInfo dirInfo, bool areSubDirsEmpty);
+
+        // return: Is this directory empty?
+        static bool WalkDirectoryTree(DirectoryInfo root, IsEmptyDirectory pred)
+        {
+            DirectoryInfo[] subDirs = root.GetDirectories();
+
+            bool areSubDirsEmpty = true;
+            foreach (DirectoryInfo dirInfo in subDirs)
+            {
+                if (false == WalkDirectoryTree(dirInfo, pred))
+                    areSubDirsEmpty = false;
+            }
+
+            bool isRootEmpty = pred(root, areSubDirsEmpty);
+            return isRootEmpty;
+        }
+
+        static bool DirHasNoFile(DirectoryInfo dirInfo)
+        {
+            FileInfo[] files = null;
+
+            try
+            {
+                files = dirInfo.GetFiles("*.*");
+                files = files.Where(x => !IsMetaFile(x.Name)).ToArray();
+            }
+            catch (Exception)
+            {
+            }
+
+            return files == null || files.Length == 0;
+        }
+
+        static string GetRelativePathFromCd(string filespec)
+        {
+            return GetRelativePath(filespec, Directory.GetCurrentDirectory());
+        }
+
+        public static string GetRelativePath(string filespec, string folder)
+        {
+            Uri pathUri = new Uri(filespec);
+            // Folders must end in a slash
+            if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                folder += Path.DirectorySeparatorChar;
+            }
+
+            Uri folderUri = new Uri(folder);
+            return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString()
+                .Replace('/', Path.DirectorySeparatorChar));
+        }
+
+        static bool IsMetaFile(string path)
+        {
+            return path.EndsWith(".meta");
+        }
+    }
+}
---
+++ Editor/DeleteEmptyFolderTool/MainWindow.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DoubleDash.DeleteEmptyDirTools
+{
+    public class MainWindow : EditorWindow
+    {
+        List<DirectoryInfo> emptyDirs;
+        Vector2 scrollPosition;
+        bool lastCleanOnSave;
+        string delayedNotiMsg;
+        
+        bool hasNoEmptyDir { get { return emptyDirs == null || emptyDirs.Count == 0; } }
+
+        const float DIR_LABEL_HEIGHT = 21;
+
+        [MenuItem("DoubleDash/Delete Empty Folders")]
+        public static void ShowWindow()
+        {
+            var w = GetWindow<MainWindow>();
+            w.titleContent.text = "Clean";
+        }
+
+        void OnEnable()
+        {
+            lastCleanOnSave = DeleteEmptyFolders.CleanOnSave;
+            DeleteEmptyFolders.OnAutoClean += Core_OnAutoClean;
+            delayedNotiMsg = "Click 'Find Empty Dirs' Button.";
+        }
+        
+        void OnDisable()
+        {
+            DeleteEmptyFolders.CleanOnSave = lastCleanOnSave;
+            DeleteEmptyFolders.OnAutoClean -= Core_OnAutoClean;
+        }
+        void Core_OnAutoClean()
+        {
+            delayedNotiMsg = "Cleaned on Save";
+        }
+
+        void OnGUI()
+        {
+            if ( delayedNotiMsg != null )
+            {
+                ShowNotification( new GUIContent( delayedNotiMsg ) );
+                delayedNotiMsg = null;
+            }
+
+            EditorGUILayout.BeginVertical();
+            {
+                EditorGUILayout.BeginHorizontal();
+                {
+                    if (GUILayout.Button("Find Empty Dirs"))
+                    {
+                        DeleteEmptyFolders.FillEmptyDirList(out emptyDirs);
+                        if (hasNoEmptyDir)
+                        {
+                            ShowNotification( new GUIContent( "No Empty Directory" ) );
+                        }
+                        else
+                        {
+                            RemoveNotification();
+                        }
+                    }
+                    if ( ColorButton( "Delete All", ! hasNoEmptyDir, Color.red ) )
+                    {
+                        DeleteEmptyFolders.DeleteAllEmptyDirAndMeta(ref emptyDirs);
+                        ShowNotification( new GUIContent( "Deleted All" ) );
+                    }
+                }
+                
+                EditorGUILayout.EndHorizontal();
+                bool cleanOnSave = GUILayout.Toggle(lastCleanOnSave, " Clean Empty Dirs Automatically On Save");
+                if (cleanOnSave != lastCleanOnSave)
+                {
+                    lastCleanOnSave = cleanOnSave;
+                    DeleteEmptyFolders.CleanOnSave = cleanOnSave;
+                }
+
+                GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
+                if ( ! hasNoEmptyDir )
+                {
+                    scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.ExpandWidth(true));
+                    {
+                        EditorGUILayout.BeginVertical();
+                        {
+#if UNITY_4_6   // and higher
+                            GUIContent folderContent = EditorGUIUtility.IconContent("Folder Icon");
+#else
+                            GUIContent folderContent = new GUIContent();
+#endif
+                            foreach (var dirInfo in emptyDirs)
+                            {
+                                UnityEngine.Object assetObj = AssetDatabase.LoadAssetAtPath( "Assets", typeof(UnityEngine.Object) );
+                                if ( null != assetObj )
+                                {
+                                    folderContent.text = DeleteEmptyFolders.GetRelativePath(dirInfo.FullName, Application.dataPath);
+                                    GUILayout.Label( folderContent, GUILayout.Height( DIR_LABEL_HEIGHT ) );
+                                }
+                            }
+                        }
+                        EditorGUILayout.EndVertical();
+                    }
+                    EditorGUILayout.EndScrollView();
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        void ColorLabel(string title, Color color)
+        {
+            Color oldColor = GUI.color;
+            //GUI.color = color;
+            GUI.enabled = false;
+            GUILayout.Label(title);
+            GUI.enabled = true;;
+            GUI.color = oldColor;
+        }
+        bool ColorButton(string title, bool enabled, Color color)
+        {
+            bool oldEnabled = GUI.enabled;
+            Color oldColor = GUI.color;
+
+            GUI.enabled = enabled;
+            GUI.color = color;
+
+            bool ret = GUILayout.Button(title);
+
+            GUI.enabled = oldEnabled;
+            GUI.color = oldColor;
+            
+            return ret;
+        }
+    }
+}
